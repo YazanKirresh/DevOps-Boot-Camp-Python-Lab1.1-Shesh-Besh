1a4c23eaf877c4147e051789a7d505ec7dfabb83
Yazan Kirresh <62175295+YazanKirresh@users.noreply.github.com>
2022-02-27 17:14:30 +0200

    Add files via upload

---
+++ 1.1 Rolling Dice.py
@@ -0,0 +1,14 @@
+import random
+
+def roll_dice():
+    print("We Rolled:")
+    print(random.randint(0, 6))
+
+
+
+print("Welcome to the trolling dice game!")
+print("In this game we will run two dices for you!")
+roll_dice()
+roll_dice()
+
+
